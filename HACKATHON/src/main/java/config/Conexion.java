6dc37e7abd37bd63b175d780a7ef487313887dd1
YancarlosHuacreCardenas <131498027+YancarlosHuacreCardenas@users.noreply.github.com>
2023-12-12 12:30:50 -0500

    Add files via upload

---
+++ HACKATHON/src/main/java/config/Conexion.java
@@ -0,0 +1,25 @@
+package config;
+
+
+public class Conexion {
+    
+    
+    Hostname =Hackathon;
+    password =(admin);
+    user = root;
+    
+            
+    Conexion con = Conexion();
+
+    private Conexion() {
+       jbdc(mysql)'//localhost:3306'
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+        
+    
+
+
+
+    
+
+}
